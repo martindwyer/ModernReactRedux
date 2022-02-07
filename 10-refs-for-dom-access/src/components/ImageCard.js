493bebcde1f0f2c8c11a1515f4bcb2b4f3e7fde9
martindwyer <martin@mdbytes.com>
2022-02-07 11:01:54 -0600

    Unit 10 Complete

--- 10-refs-for-dom-access/src/components/ImageCard.js
+++ 10-refs-for-dom-access/src/components/ImageCard.js
@@ -13,20 +13,15 @@ class ImageCard extends React.Component {
 
   setSpans = () => {
     const height = this.imageRef.current.clientHeight;
-    const spans = Math.ceil(height / 150);
+    const spans = Math.ceil(height / 10);
     this.setState({ spans });
   };
   render() {
     const { description, urls } = this.props.image;
 
     return (
-      <div>
-        <img
-          ref={this.imageRef}
-          src={urls.regular}
-          alt={description}
-          style={{ gridRowEnd: `span ${this.state.spans}` }}
-        />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
   }
